e164e0b34d56de00ec236ff2f3ca607f35c8fd0b
Unknown <bonniezhengg@hotmail.com>
2017-09-02 00:54:08 -0700

    init commit
    - secureTextEntry
    - removed autoCaps/autoCorrects
    - added loginform, TouchableOpacity effect

---
+++ YesVancouverApp/src/components/Login/LoginForm.js
@@ -0,0 +1,101 @@
+
+import React, { Component } from 'react';
+
+import {StyleSheet,View,TextInput, TouchableOpacity, Text, StatusBar} from 'react-native';
+
+export default class LoginForm extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar
+          barStyle="dark-content"
+          />
+
+        <TextInput 
+        placeholder="Email"
+        placeholderTextColor="rgba(128,128,128,0.7)"
+          returnKeyType="next"
+          onSubmitEditing={()=> this.passwordInput.focus()}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          style= {styles.input} 
+          />
+        <TextInput 
+        placeholder="Password"
+        placeholderTextColor='rgba(128,128,128,0.7)'
+        returnKeyType="go"
+        secureTextEntry 
+        style={styles.input}
+        ref={(input) => this.passwordInput = input}
+        />
+
+        <TouchableOpacity style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.passContainer}>
+          <Text style={styles.forgotPassText}>I forgot my password</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // padding: 30,
+    paddingHorizontal: 50,
+    // borderLeftWidth: 50,
+    // borderRightWidth: 50,
+    // height: 60
+    
+  
+  },
+
+  input: {
+    height: 40,
+    backgroundColor:'rgba(255,255,255,0.7)',
+    marginBottom:20,
+    // marginTop: 20,
+
+    paddingHorizontal:5,
+    borderBottomWidth:1,
+
+  },
+
+  buttonContainer:{
+    backgroundColor: '#ff0066',
+    paddingVertical: 10,
+    marginTop: 30
+
+  },
+
+  buttonText:{
+    textAlign:'center',
+    color:'#FFFFFF',
+    fontWeight: '600',
+    fontSize: 19,
+    padding: 5
+    
+
+    
+
+  },
+
+  passContainer:{
+    paddingVertical: 50
+    
+  },
+
+  forgotPassText:{
+    textAlign: 'center',
+    fontSize: 16,
+    color: '#808080',
+    
+  }
+
+ 
+
+});
+
---
+++ YesVancouverApp/src/components/Login/login.js
@@ -0,0 +1,36 @@
+
+import React, { Component } from 'react';
+import {StyleSheet,View,Image, Text, KeyboardAvoidingView} from 'react-native';
+import LoginForm from './LoginForm';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      
+
+        <View style={styles.bannerContainer}>
+          
+        </View>
+        <View style= {styles.formContainer}>
+        <LoginForm/>
+        </View>
+
+
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+
+  },
+
+  bannerContainer:{
+    paddingBottom: 200
+  }
+
+});
+

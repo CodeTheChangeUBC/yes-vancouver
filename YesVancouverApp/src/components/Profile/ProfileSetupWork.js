1532df125298a6f1450714dae93e7148f4e5cb87
Otto Lau <otto.lau@hotmail.com>
2017-11-26 17:54:16 -0800

    Clean up styles in ProfileSetupWork

--- YesVancouverApp/src/components/Profile/ProfileSetupWork.js
+++ YesVancouverApp/src/components/Profile/ProfileSetupWork.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Image, SectionList, Text } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 import Header from '../Navigation/Header';
 
 
@@ -7,48 +7,19 @@ export default class ProfileSetupWork extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={{flex:1}}>
+                <View style={styles.headerContainer}>
                     <Header style={styles.header}/>
                     
-                    <View style={{
-                        position: 'absolute',
-                        width: '100%',
-                        height: '100%',
-                        flexDirection: 'row',
-                        justifyContent: 'center',
-                        alignItems: 'center'
-                    }}>
-                        <View style={
-                            {
-                            flex: 1,
-                            alignItems: 'center',
-                            borderWidth: 2,
-                            borderColor: 'black',
-                            }
-                        }>
+                    <View style={styles.headerIconContainer}>
+                        <View style={styles.backArrowContainer}>
                             <Image source={require('../../images/Header/White-arrow@3x.png')}/>
                         </View>
 
-                        <View style={
-                            {
-                            flex: 4,
-                            alignItems: 'center',
-                            borderWidth: 2,
-                            borderColor: 'black',
-                            }
-                        }>
+                        <View style={styles.pageIndicatorContainer}>
                             <Image source={require('../../images/Login-Signup/Pagination/Pagination1@3x.png')}/>
                         </View>
 
-                        <View style={
-                            {
-                            flex: 1,
-                            borderWidth: 2,
-                            borderColor: 'black',
-                            }
-                        }>
-                        </View>
-
+                        <View style={styles.backArrowContainer}></View>
                     </View>
                 </View>
                 <View style={styles.content}>
@@ -63,10 +34,29 @@ const styles = StyleSheet.create({
     container: {
         flex: 1
     },
+    headerContainer: {
+        flex: 1
+    },
     header: {
         flex: 1,
         width: null,
         height: null
+    },
+    headerIconContainer: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    backArrowContainer: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    pageIndicatorContainer: {
+        flex: 4,
+        alignItems: 'center'
     },
     content: {
         flex: 7,

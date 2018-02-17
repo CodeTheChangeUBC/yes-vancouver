8580e406acfece14eee21ce5c7dea094a5bf30df
Otto Lau <otto.lau@hotmail.com>
2018-02-17 12:01:16 -0800

    Use stacknav in eventsview, add header mode disable

--- YesVancouverApp/src/components/Events/EventsView.js
+++ YesVancouverApp/src/components/Events/EventsView.js
@@ -5,41 +5,41 @@ import EventsList from './EventsList'
 import EventsDetails from './EventsDetails'
 
 
-export default class EventsView extends Component {
-    // static navigationOptions = {
-    //     tabBarLabel: 'EventsView',
-    //     title: 'EventsView',
-    //     tabBarIcon: ({ focused, tintColor }) => focused ?
-    //     ( <Image 
-    //         source={require('../../images/NavBar/Calendar-icon-white-3x.png')}
-    //         resizeMode="contain"
-    //         style={{height:30}}/> 
-    //     ) :
-    //     ( <Image 
-    //         source={require('../../images/NavBar/Calendar-icon-orange-3x.png')}
-    //         resizeMode="contain"
-    //         style={{height:30}}/>
-    //     )
-    // }
+// export default class EventsView extends Component {
+//     // static navigationOptions = {
+//     //     tabBarLabel: 'EventsView',
+//     //     title: 'EventsView',
+//     //     tabBarIcon: ({ focused, tintColor }) => focused ?
+//     //     ( <Image 
+//     //         source={require('../../images/NavBar/Calendar-icon-white-3x.png')}
+//     //         resizeMode="contain"
+//     //         style={{height:30}}/> 
+//     //     ) :
+//     //     ( <Image 
+//     //         source={require('../../images/NavBar/Calendar-icon-orange-3x.png')}
+//     //         resizeMode="contain"
+//     //         style={{height:30}}/>
+//     //     )
+//     // }
 
-    render() {
-        return (
-             <EventsList/>
-            //<EventsDetails/>
-        );
-    }
-}
+//     render() {
+//         return (
+//              <EventsList/>
+//             //<EventsDetails/>
+//         );
+//     }
+// }
 
-const stackNav = StackNavigator({
+const EventsView = StackNavigator({
     EventsList: {
         screen: EventsList,
         navigationOptions:({navigation}) => ({
             title: "EventsList",
-            headerLeft:(
-              <TouchableOpacity onPress={() => navigation.navigate("DrawerOpen")}>
-                <IOSIcon name="ios-menu" size={30} />
-              </TouchableOpacity>
-            ),
+            // headerLeft:(
+            //   <TouchableOpacity onPress={() => navigation.navigate("DrawerOpen")}>
+            //     <IOSIcon name="ios-menu" size={30} />
+            //   </TouchableOpacity>
+            // ),
             headerStyle: { paddingRight: 10, paddingLeft: 10 }
         })
     },
@@ -49,4 +49,13 @@ const stackNav = StackNavigator({
             title: "Detail",
         })
     }
-})
+},
+// {
+//     headerMode: 'none',
+//     navigationOptions: {
+//         headerVisible: false,
+//     }
+// }
+)
+
+export default EventsView

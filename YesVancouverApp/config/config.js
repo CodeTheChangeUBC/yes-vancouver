4fd81702b70c9b483ab6f41ad1e25352b805a5a3
Otto Lau <otto.lau@hotmail.com>
2018-07-09 19:57:53 -0700

    Update sample config file

--- YesVancouverApp/config/config.js
+++ YesVancouverApp/config/config.js
@@ -2,5 +2,10 @@
 export const ClientSecrets = {
     ACCOUNT_NUM : '123456',
     API_USERNAME: '123456789',
-    API_PASSWORD: '123456789'
+    API_PASSWORD: '123456789',
+
+    CLIENT_ID: '123456789',
+    CLIENT_SECRET: '123456789',
+
+    FQDN: 'https://<wild_apricot_name>.wildapricot.org'
 }

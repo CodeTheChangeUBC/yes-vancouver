57ecfc989f94947eba4c44a2d7d3257e1872aaf7
Otto Lau <otto.lau@hotmail.com>
2018-07-13 22:08:19 -0700

    Add header image, back button to StackNavigator's header

--- YesVancouverApp/src/components/Navigation/Navigation.js
+++ YesVancouverApp/src/components/Navigation/Navigation.js
@@ -2,6 +2,10 @@
  * Created by joycheng on 2017-10-21.
  */
 
+import React, { Component } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { StackNavigator } from 'react-navigation'
+
 import Login from '../Login/LoginForm'
 import HomeScreen from '../HomeScreen/HomeScreen'
 import SignUp from '../SignUp/SignUp'
@@ -10,11 +14,13 @@ import ProfileSetupSocial from '../Profile/Old Views/ProfileSetupSocial'
 import ProfileSetupWork from '../Profile/Old Views/ProfileSetupWork'
 import ResetPassword from '../ResetPassword/ResetPassword'
 import NavBar from './NavBar'
-import { StackNavigator } from 'react-navigation'
 import Aveda from '../Perks/aveda'
 import PerksView from '../Perks/PerksView'
 import PerksList from '../Perks/PerksList'
-import EditProfile from "../Profile/EditProfile";
+import EditProfile from "../Profile/EditProfile"
+import Header from './Header'
+import Expo from 'expo'
+
 
 const Navigation = StackNavigator({
     HomeScreen : {screen : HomeScreen},
@@ -30,12 +36,41 @@ const Navigation = StackNavigator({
     PerksView: {screen: PerksView},
     PerksList: {screen: PerksList},
 },
-// {
-//     headerMode: 'none',
-//     navigationOptions: {
-//       headerVisible: false,
-//     }
-// }
-)
+{
+    navigationOptions: ({ navigation }) => ({
+        headerStyle:{
+            marginTop: Expo.Constants.statusBarHeight,
+        },
+        headerVisible: true,
+        headerTitleStyle: {
+            flex: 1,
+            textAlign: 'center',
+            alignSelf: 'center'
+        },
+        headerBackImage: require('../../images/Header/White-arrow-3x.png'),
+        headerBackTitle: null,
+        headerTintColor: 'white',
+        headerTransparent: false,
+        headerBackground: 
+            <View style={styles.headerContainer}>
+                <Header style={styles.header}/>
+            </View>,
+        // headerLeft: (Platform.OS === 'android' && ()) ? <View /> : null,
+        // <View style={{backgroundColor:'transparent'}}/>,
+        // headerRight: <View />,
+    })
+})
 
 export default Navigation
+
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        flex: 1
+    },
+    header: {
+        flex: 1,
+        width: null,
+        height: null
+    }
+})

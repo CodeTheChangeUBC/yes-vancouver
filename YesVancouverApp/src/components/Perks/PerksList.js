9fbc6146a71172349d44665a727363c025c99e8e
Otto Lau <otto.lau@hotmail.com>
2018-07-14 21:58:39 -0700

    Remove duplicate header in PerksPartner

--- YesVancouverApp/src/components/Perks/PerksList.js
+++ YesVancouverApp/src/components/Perks/PerksList.js
@@ -1,31 +1,26 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Image, SectionList, Text, FlatList, TouchableHighlight } from 'react-native';
-import Header from '../Navigation/Header';
-import PerksItem from './PerksItem';
+import React, { Component } from 'react'
+import { FlatList, Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native'
+import PerksItem from './PerksItem'
 
-import { List, ListItem } from "react-native-elements";
-
+import { List, ListItem } from "react-native-elements"
 
 
 export default class PerksList extends Component {
 
     static navigationOptions = {
         title: "PerksList",
-    };
-
-    constructor(props)  {
-        super(props);
     }
 
-
+    constructor(props)  {
+        super(props)
+    }
 
     goToNextScreen = () => {
-        let {navigate} = this.props.navigation;
-        return navigate('Aveda');
+        let {navigate} = this.props.navigation
+        return navigate('Aveda')
     }
 
     renderItem = ({item}) => {
-
         return (
 
             <TouchableHighlight onPress={() => this.goToNextScreen()}>
@@ -34,30 +29,12 @@ export default class PerksList extends Component {
                 </View>
             </TouchableHighlight>
         //    <Text>{image}</Text>
-        );
+        )
     }
 
     render() {
-
         return (
             <View style={styles.container}>
-                <View style={styles.headerContainer}>
-                    <Header style={styles.header}/>
-                    <View style={styles.headerIconContainer}>
-                        <View style={styles.menuContainer}></View>
-                        <View style={styles.perksPartnerTitleContainer}>
-                            <Text style={styles.perksPartnerTitleText}>
-                                Perks Partners
-                            </Text>
-                        </View>
-                        <View style={styles.menuContainer}>
-                            <Image source={require('../../images/Header/Menu-icon-white-3x.png')}
-                                resizeMode="contain"
-                                style={{height:30}}/>
-                        </View>
-                    </View>
-                </View>
-                <View style={styles.content}>
                 <View style={styles.sortBar}>
                     <View style={styles.aToZTab}>
                         <Image 
@@ -78,69 +55,31 @@ export default class PerksList extends Component {
                         <Text style={styles.favouritesText}>Favourites</Text>
                     </View>
                 </View>
-                    <List>
-                        <FlatList
-                            data={[
-                                {key: 'Aveda',
-                                    image: require('../../images/Perks/Aveda.png')},
-                                {key: 'Camp-Tech',
-                                    image: require('../../images/Perks/Camp-Tech.png')},
-                                {key: 'FloatHouse',
-                                    image: require('../../images/Perks/Floathouse.png')},
-                                {key: 'Red-Academy',
-                                    image: require('../../images/Perks/Red-Academy.png')},
-                                {key: 'Spin-Society',
-                                    image: require('../../images/Perks/Spin-Society.png')},
-
-
-                            ]}
-                            renderItem={this.renderItem}
-                        />
-                    </List>
-
-                </View>
+                <List>
+                    <FlatList
+                        data={[
+                            {key: 'Aveda',
+                                image: require('../../images/Perks/Aveda.png')},
+                            {key: 'Camp-Tech',
+                                image: require('../../images/Perks/Camp-Tech.png')},
+                            {key: 'FloatHouse',
+                                image: require('../../images/Perks/Floathouse.png')},
+                            {key: 'Red-Academy',
+                                image: require('../../images/Perks/Red-Academy.png')},
+                            {key: 'Spin-Society',
+                                image: require('../../images/Perks/Spin-Society.png')},
+                        ]}
+                        renderItem={this.renderItem}
+                    />
+                </List>
             </View>
-        );
+        )
     }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1
-    },
-    content: {
-        flex: 7
-    },
-    headerContainer: {
-        flex: 1
-    },
-    header: {
-        flex: 1,
-        width: null,
-        height: null
-    },
-    headerIconContainer: {
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    menuContainer: {
-        flex: 1,
-        alignItems: 'center'
-    },
-    perksPartnerTitleContainer: {
-        flex: 4,
-        alignItems: 'center'
-    },
-    perksPartnerTitleText: {
-        fontFamily: 'alternate-gothic-no3-d-regular',
-        fontSize: 24,
-        color: '#FFFFFF',
-        backgroundColor: 'transparent',
-        textAlign: 'center'
     },
     sortBar: {
         flexDirection: 'row',
@@ -189,4 +128,4 @@ const styles = StyleSheet.create({
         fontFamily: 'alternate-gothic-no3-d-regular',
         fontSize: 24
     }
-});
+})
